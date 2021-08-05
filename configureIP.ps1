a29c294128f955162a0661eecb1def1263b2bc55
Darren Fitzpatrick <mrdarrenm@gmail.com>
2021-08-05 23:07:10 +0100

    add configureIP.ps1

---
+++ configureIP.ps1
@@ -0,0 +1,23 @@
+$IP = "172.17.2.40"
+$MaskBits = 24 # This means subnet mask = 255.255.255.0
+$Gateway = "172.17.2.1"
+$Dns = "172.17.2.1"
+$Dns2 = "8.8.4.4"
+$IPType = "IPv4"
+# Retrieve the network adapter that you want to configure
+$adapter = Get-NetAdapter | ? {$_.Status -eq "up"}
+# Remove any existing IP, gateway from our ipv4 adapter
+If (($adapter | Get-NetIPConfiguration).IPv4Address.IPAddress) {
+ $adapter | Remove-NetIPAddress -AddressFamily $IPType -Confirm:$false
+}
+If (($adapter | Get-NetIPConfiguration).Ipv4DefaultGateway) {
+ $adapter | Remove-NetRoute -AddressFamily $IPType -Confirm:$false
+}
+ # Configure the IP address and default gateway
+$adapter | New-NetIPAddress `
+ -AddressFamily $IPType `
+ -IPAddress $IP `
+ -PrefixLength $MaskBits `
+ -DefaultGateway $Gateway
+# Configure the DNS client server IP addresses
+$adapter | Set-DnsClientServerAddress -ServerAddresses ($Dns,$Dns2)
