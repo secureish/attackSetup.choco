debbb6bb84e3fe6bc2f7d2915650c6f67cc2d389
Darren Fitzpatrick <mrdarrenm@gmail.com>
2021-08-06 17:40:58 +0100

    add git install

--- installBoxstarter.ps1
+++ installBoxstarter.ps1
@@ -2,3 +2,4 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 iex ((New-Object System.Net.WebClient).DownloadString('https://boxstarter.org/bootstrapper.ps1'))
 Get-Boxstarter -Force
+choco install git
