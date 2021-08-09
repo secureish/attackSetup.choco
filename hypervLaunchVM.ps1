fa01c4f75d9780e749047f6dd82a91e2e8e256ec
Darren Fitzpatrick <mrdarrenm@gmail.com>
2021-08-09 20:03:47 +0100

    fixed vmsetup script

--- hypervLaunchVM.ps1
+++ hypervLaunchVM.ps1
@@ -1,13 +1,14 @@
-$vmname
-$vmpath = "C:\VMs\"+$vmname
-$hddath = $vmpath+"hdds\"
+$vmname = "kali"
+$vmpath = "C:\VMs\"+$vmname+"\"
+$hddpath = $vmpath+"hdds\"
 $isospath = "C:\VMs\isos\"
-$isoname = "windows10x64.iso"
+$isoname = "kali-linux-2021-W31-installer-amd64.iso"
+$vhdpath = $hddpath+$vmname+".vhdx"
 
 New-VM -Name $vmname -MemoryStartupBytes 512MB -Path $vmpath
-New-VHD -Path D:\ws2012.localDCDC.vhdx -SizeBytes 60GB -Dynamic
-Add-VMHardDiskDrive -VMName $vmname -Path $hddpath+$vmname+".vhdx" 
-Set-VMDvdDrive -VMName $vmname -ControllerNumber 1 -Path $isospath+$isoname
-Set-VMMemory -VMName $vmname -DynamicMemoryEnabled $true -StartupBytes 512MB -MinimumByte 512
+New-VHD -Path $vhdpath -SizeBytes 60GB -Dynamic
+Add-VMHardDiskDrive -VMName $vmname -Path $vhdpath
+Set-VMDvdDrive -VMName $vmname -ControllerNumber 1 -Path $isospath$isoname
+Set-VMMemory -VMName $vmname -DynamicMemoryEnabled $true -StartupBytes 12GB
 Start-VM –Name $vmname
 Get-VM $vmname
