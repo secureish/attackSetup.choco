5fc77a5ab8420de279307c05ae8a2ef268c82793
secureish <secureish@users.noreply.github.com>
2021-08-10 21:36:02 +0100

    Update hypervLaunchVM.ps1

--- hypervLaunchVM.ps1
+++ hypervLaunchVM.ps1
@@ -1,14 +1,14 @@
-$vmname = "kali"
+$vmname = "cli1"
 $vmpath = "C:\VMs\"+$vmname+"\"
 $hddpath = $vmpath+"hdds\"
 $isospath = "C:\VMs\isos\"
-$isoname = "kali-linux-2021-W31-installer-amd64.iso"
+$isoname = "Windows_Server_2016_Datacenter_EVAL_en-us_14393_refresh.ISO"
 $vhdpath = $hddpath+$vmname+".vhdx"
 
 New-VM -Name $vmname -MemoryStartupBytes 512MB -Path $vmpath
-New-VHD -Path $vhdpath -SizeBytes 60GB -Dynamic
+New-VHD -Path $vhdpath -SizeBytes 40GB -Dynamic
 Add-VMHardDiskDrive -VMName $vmname -Path $vhdpath
 Set-VMDvdDrive -VMName $vmname -ControllerNumber 1 -Path $isospath$isoname
-Set-VMMemory -VMName $vmname -DynamicMemoryEnabled $true -StartupBytes 12GB
-Start-VM –Name $vmname
+Set-VMMemory -VMName $vmname -DynamicMemoryEnabled $true -StartupBytes 8GB
+Start-VM -Name $vmname
 Get-VM $vmname
