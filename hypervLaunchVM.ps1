77ce45ef7a6b0487a2a96b59d893d51abaffb126
secureish <secureish@users.noreply.github.com>
2021-08-10 21:40:08 +0100

    Update hypervLaunchVM.ps1

--- hypervLaunchVM.ps1
+++ hypervLaunchVM.ps1
@@ -5,7 +5,7 @@ $isospath = "C:\VMs\isos\"
 $isoname = "19043.928.210409-1212.21h1_release_svc_refresh_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_en-gb.iso"
 $vhdpath = $hddpath+$vmname+".vhdx"
 
-New-VM -Name $vmname -MemoryStartupBytes 512MB -Path $vmpath
+New-VM -Name $vmname -MemoryStartupBytes 4GB -Path $vmpath
 New-VHD -Path $vhdpath -SizeBytes 40GB -Dynamic
 Add-VMHardDiskDrive -VMName $vmname -Path $vhdpath
 Set-VMDvdDrive -VMName $vmname -ControllerNumber 1 -Path $isospath$isoname
