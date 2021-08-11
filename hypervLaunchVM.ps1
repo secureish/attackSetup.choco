6a8b925f0bd055ccdb323483d175a9a6b6ccfc70
secureish <secureish@users.noreply.github.com>
2021-08-11 21:58:41 +0100

    Update hypervLaunchVM.ps1

--- hypervLaunchVM.ps1
+++ hypervLaunchVM.ps1
@@ -5,10 +5,11 @@ $isospath = "C:\VMs\isos\"
 $isoname = "19043.928.210409-1212.21h1_release_svc_refresh_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_en-gb.iso"
 $vhdpath = $hddpath+$vmname+".vhdx"
 
-New-VM -Name $vmname -MemoryStartupBytes 4GB -Path $vmpath
-New-VHD -Path $vhdpath -SizeBytes 40GB -Dynamic
+New-VM -Name $vmname -MemoryStartupBytes 8GB -Path $vmpath
+New-VHD -Path $vhdpath -SizeBytes 45GB -Dynamic
 Add-VMHardDiskDrive -VMName $vmname -Path $vhdpath
 Set-VMDvdDrive -VMName $vmname -ControllerNumber 1 -Path $isospath$isoname
 Set-VMMemory -VMName $vmname -DynamicMemoryEnabled $true -StartupBytes 8GB
+Add-VMNetworkAdapter -VMName $vmname -Name "ExtSwitch"
 Start-VM -Name $vmname
 Get-VM $vmname
